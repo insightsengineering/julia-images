939356609ebc8f71fb46ebcf4a79a7efc54ee9bb
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2024-03-25 14:06:45 +0100

    Update

--- scripts/install_sysdeps.sh
+++ scripts/install_sysdeps.sh
@@ -2,22 +2,55 @@
 
 set -e
 
-# Takes in the distribution as the first argument.
-distribution="$1"
+# Takes in the destination image name as the first argument.
+destination_image_name="$1"
 
-echo "Distribution = $distribution"
+echo "DESTINATION_IMAGE_NAME = $destination_image_name"
+
+declare -A pkgs_to_install
+
+shared_deps="\
+wget \
+"
+
+pkgs_to_install["julia"]="${shared_deps}"
+
+pkgs_to_install["julia-vscode"]="${shared_deps} \
+curl \
+ssh \
+git \
+vim \
+less \
+nano \
+"
 
 mkdir /root/.config
-apt-get update
-apt-get install -y sudo git wget htop
-apt-get clean
-apt-get autoremove -y
-rm -rf /var/lib/apt/lists/*
-echo "DEBIAN_FRONTEND=$DEBIAN_FRONTEND" >> /etc/profile
-echo "LC_ALL=$LC_ALL" >> /etc/profile
-echo "PATH=$PATH" >> /etc/profile
+
+export DEBIAN_FRONTEND=noninteractive
+export ACCEPT_EULA=Y
+apt-get update -y
+apt-get install -y -q ${shared_deps}
+apt-get install -y ${pkgs_to_install["${destination_image_name}"]}
+
+# Install quarto
+ARCH=$(dpkg --print-architecture)
+QUARTO_DL_URL=$(wget -qO- https://api.github.com/repos/quarto-dev/quarto-cli/releases/latest | grep -oP "(?<=\"browser_download_url\":\s\")https.*${ARCH}\.deb")
+wget -q "${QUARTO_DL_URL}" -O quarto-"${ARCH}".deb
+dpkg -i quarto-"${ARCH}".deb
+quarto check install
 
 if [ "$distribution" == "julia-vscode" ]; then
   # Install VS Code server.
   wget --no-check-certificate https://code-server.dev/install.sh -O - | sh
 fi
+
+# Install security patches
+unattended-upgrade -v
+
+# Clean up
+apt-get autoremove -y
+apt-get autoclean -y
+rm -rf /var/lib/apt/lists/* quarto-"${ARCH}".deb
+
+echo "LC_ALL=$LC_ALL" >> /etc/profile
+echo "PATH=$PATH" >> /etc/profile
