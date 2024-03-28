72b32b7aa919e3dd4a5d05808874e0173afffe68
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2024-03-28 16:39:44 +0100

    Install VS Code extensions

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -7,6 +7,11 @@ destination_image_name="$1"
 
 echo "DESTINATION_IMAGE_NAME = $destination_image_name"
 
+# List of VS Code Server extensions.
+declare -a extension_list=(
+  "julialang.language-julia"
+)
+
 # Install quarto
 ARCH=$(dpkg --print-architecture)
 QUARTO_DL_URL=$(wget -qO- https://api.github.com/repos/quarto-dev/quarto-cli/releases/latest | grep -oP "(?<=\"browser_download_url\":\s\")https.*${ARCH}\.deb")
@@ -16,7 +21,13 @@ quarto check install
 
 if [ "$destination_image_name" == "julia-vscode" ]; then
   # Install VS Code server.
+  echo "Installing Code Server..."
   wget --no-check-certificate https://code-server.dev/install.sh -O - | sh
+  echo "Installing Code Server extensions..."
+  for extension_name in "${extension_list[@]}"
+  do
+    code-server --install-extension $extension_name
+  done
   cp /init-vscode /init
 fi
 
